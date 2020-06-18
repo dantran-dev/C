29f737aead3ce1bc6e7d2d2fb826537618f4bf48
Dan Tran <dantr.qld@gmail.com>
2020-06-19 03:06:39 +1000

    Insert new node into the middle

--- hello.c
+++ hello.c
@@ -26,7 +26,7 @@ void printList(node_t *head) // accept the head as where to start with
 int main()
 {
   
-node_t n1, n2, n3, n4;    // initialise 3 nodes
+node_t n1, n2, n3;   // initialise 3 nodes
 node_t *head;         //the path to the first node
 /*
 ** Set value for each node
@@ -34,15 +34,23 @@ node_t *head;         //the path to the first node
 n1.value = 18;        
 n2.value = 73;
 n3.value = 61;
-n4.value = 17;
+
 
 /*Link all the node together*/
 
 head = &n1; // access to the list by the node 3rd
 n1.next = &n2;
 n2.next = &n3;
-n3.next = &n4;
-n4.next = NULL;  // the end of the list
+n3.next = NULL; // the end of the list
+
+/*Create a new node and insert it in the middle*/
+
+node_t n4;
+n4.value = 69;
+
+n4.next = &n3;    // append new node to the next node
+n2.next = &n4;    // update old node address by new node address
+
 
 printList(head);
 return 0;
