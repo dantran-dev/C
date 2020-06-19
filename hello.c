a1e4d1337efbeb8dd9d5836635dc5da5b64cfda0
Dan Tran <dantr.qld@gmail.com>
2020-06-19 18:55:42 +1000

    change entire file content

--- hello.c
+++ hello.c
@@ -1,58 +1,42 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 struct node
 {
-    /* data */
-    int value;           // actual value will be stored in the node
-    struct node *next;   // hold the address to the next pointer
-
+    int value;
+    struct node *next;
 };
 
 typedef struct node node_t;
 
-void printList(node_t *head) // accept the head as where to start with
-{
-    node_t *temp = head;
-    while (temp!=NULL)
-    {
-        printf("-%d value located at the address %p\n",temp->value, &temp);
-        temp = temp->next;
+node_t *head;
+node_t *curr;
 
-    }
+node_t *createList(int value)
+{
     
 }
 
-node_t *createNewNode(int value)
-{
-    node_t *result = malloc(sizeof(node_t));
-    result->value = value;
-    result->next = NULL;
-    return result;
-
-}
-
-node_t *insertInFront(node_t *head, node_t *nodeToInsert)
-{
-    nodeToInsert->next = head;
-    return nodeToInsert;
-}
 
 int main()
-{
-  
-node_t *head;         //the path to the first node
-node_t *temp;
+{   
+    /*
+    note_t n1,n2;
 
-for (int i = 0; i < 5; i++)
-{
-    temp = createNewNode(i);
-    head = insertInFront(head, temp);
-}
+    n1.value = 8;
+    n1.next = &n1;
 
 
-printList(head);
-return 0;
+    n2.value = 93;
+    n2.next = NULL;
 
+    printf("%d\n", n1.value);
+
+    printf("%d\n",n2.value);
+    */
+
+
+    return 0;
+
+    
 }
