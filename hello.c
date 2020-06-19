1184b75743fd7dd4ec29151a31e5ad83a840f697
Dan Tran <dantr.qld@gmail.com>
2020-06-19 14:30:04 +1000

    created functions for insert into the linked list

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
+
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -16,40 +17,39 @@ void printList(node_t *head) // accept the head as where to start with
     node_t *temp = head;
     while (temp!=NULL)
     {
-        printf("-%d located at the address %p\n",temp->value, &temp);
+        printf("-%d value located at the address %p\n",temp->value, &temp);
         temp = temp->next;
 
     }
     
 }
 
+node_t *createNewNode(int value)
+{
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+
+}
+
+node_t *insertInFront(node_t *head, node_t *nodeToInsert)
+{
+    nodeToInsert->next = head;
+    return nodeToInsert;
+}
+
 int main()
 {
   
-node_t n1, n2, n3;   // initialise 3 nodes
 node_t *head;         //the path to the first node
-/*
-** Set value for each node
-*/
-n1.value = 18;        
-n2.value = 73;
-n3.value = 61;
+node_t *temp;
 
-
-/*Link all the node together*/
-
-head = &n1; // access to the list by the node 3rd
-n1.next = &n2;
-n2.next = &n3;
-n3.next = NULL; // the end of the list
-
-/*Create a new node and insert it in the middle*/
-
-node_t n4;
-n4.value = 69;
-
-n4.next = &n3;    // append new node to the next node
-n2.next = &n4;    // update old node address by new node address
+for (int i = 0; i < 5; i++)
+{
+    temp = createNewNode(i);
+    head = insertInFront(head, temp);
+}
 
 
 printList(head);
