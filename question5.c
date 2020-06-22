2e4ee65972df1b96e073a958dc267fd15a255941
Dan Tran <dantr.qld@gmail.com>
2020-06-22 21:52:38 +1000

    Added more files

--- question5.c
+++ question5.c
@@ -52,11 +52,93 @@ void init_collection(collection *cp)
 {
     cp = (struct collection*)malloc(sizeof(collection));
     
-
 }
+
+// add marble to the front of collection c
 
 void add_marble(collection c, marble m)
 {
+   
+    //create a node with the marble in it
+    collection marble_node;
+   
+  
+    //create marble node with marble inside it
+    init_node(&marble_node, m);
+
+    //link the new marble node to the next node
+    marble_node = club;
+
+    //update the head
+    club = marble_node;
+
+    
+}
+
+void display_marbles(collection c, content k)
+{
+
+    c = club;
+
+    //loop through collection c then find the node that has content k
+
+    while ((c!=NULL) && (strcmp(get_look(c)= k)))
+    {
+        printf("%s", (char*)get_look(c));
+        c = c->next;
+    }
     
 
+}
+
+int display_range(collection c, double min_diameter, double max_diameter)
+{
+
+    c = club;
+
+    while (c!=NULL)
+    {
+        if ((get_diameter(c)>=min_diameter) && get_diameter(c) <=max_diameter)
+        {
+            
+            printf("%s \n"), (char*)get_look(c);
+
+        }
+
+        c = c->next;
+    }
+    
+}
+
+void remove(collection c)
+{
+
+ collection *temp, *prev;
+
+ temp = club;
+
+ int old = 0;
+
+ if (temp!=NULL && get_age(temp)==0)
+ {
+    
+    club = temp->next;    //change head
+    free(temp);           // delete old head
+    return;
+ }
+
+ //search for the node marble to be deleted
+ // keep track of the previous node as we need to update its next address
+
+ while (temp!=NULL && get_age(temp)!=0)
+ {
+     prev = temp;
+     temp = get_next(temp);
+ }
+ 
+ //Unlink the node from the linked list
+
+ prev->next = temp->next;
+ free(temp);  //Free memory
+ 
 }
